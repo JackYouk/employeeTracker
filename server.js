12524ec6b4b2613a4c9649a3e7329db1d8576f60
JackYouk <jackyoukstetter1@gmail.com>
2022-07-20 20:59:44 -0700

    built server w/ express, added routes folders+files, added schema, added empty files for models and db connection

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const {
+    Department,
+    Employee,
+    Role,
+} = require('./model');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`server up @ ${PORT}`));
+})
+
+
